f970648b1652534b5662a4d2fa985d9b871bf6f8
Amaury Balmer <amaury@beapi.fr>
2023-04-06 06:11:32 +0200

    add test for env, add link for backend

--- functions.php
+++ functions.php
@@ -1,44 +1,46 @@
 <?php
-add_action('admin_menu', 'add_page_template' );
+add_action( 'admin_menu', 'add_page_template' );
 
 function add_page_template() {
- 	add_theme_page('Redirection settings', __('Redirection settings', 'redirection'), 'manage_options', 'redirection-theme-settings', 'redirection_theme_page' );
+	add_theme_page( 'Redirection settings', __( 'Redirection settings', 'redirection' ), 'manage_options', 'redirection-theme-settings', 'redirection_theme_page' );
 }
 
 function redirection_theme_page() {
 	$text = '';
-	if ( isset($_POST['update_theme_options']) ) {
-		if ( 
-			! isset( $_POST['update_theme_options_field'] ) 
-			|| ! wp_verify_nonce( $_POST['update_theme_options_field'], 'update_theme_options' ) 
+	if ( isset( $_POST['update_theme_options'] ) ) {
+		if (
+			! isset( $_POST['update_theme_options_field'] )
+			|| ! wp_verify_nonce( $_POST['update_theme_options_field'], 'update_theme_options' )
 		) {
-			wp_die('Sorry, your nonce did not verify.');
+			wp_die( 'Sorry, your nonce did not verify.' );
 		}
 
-		$_POST['redirection_url'] = stripslashes($_POST['redirection_url']);
-		update_option('redirection_url', sanitize_text_field( $_POST['redirection_url'] ) );
+		$_POST['redirection_url'] = stripslashes( $_POST['redirection_url'] );
+		update_option( 'redirection_url', sanitize_text_field( $_POST['redirection_url'] ) );
 
-		$text =  '<div id="setting-error-settings_updated" class="updated settings-error notice is-dismissible"><p><strong>Settings saved.</strong></p></div>';
+		$text = '<div id="setting-error-settings_updated" class="updated settings-error notice is-dismissible"><p><strong>Settings saved.</strong></p></div>';
 	}
 
-	$redirection = get_option('redirection_url');
+	$redirection = get_option( 'redirection_url' );
 	?>
 	<div class="wrap">
-		<h2><?php _e('Redirection settings', 'redirection'); ?></h2>
+		<h2><?php _e( 'Redirection settings', 'redirection' ); ?></h2>
 		<?php echo $text; ?>
-		<form action="" method="POST"> 
+		<form action="" method="POST">
 			<table class="form-table">
 				<tr>
-					<th scope="row"><?php _e('Redirection URL', 'redirection'); ?></th>
+					<th scope="row"><?php _e( 'Redirection URL', 'redirection' ); ?></th>
 					<td>
-						<input type="text" name="redirection_url" value="<?php echo esc_attr($redirection);?>" class="widefat" />
+						<input type="text" name="redirection_url" value="<?php echo esc_attr( $redirection ); ?>"
+						       class="widefat"/>
 					</td>
-				</tr>	
+				</tr>
 			</table>
 
 			<?php wp_nonce_field( 'update_theme_options', 'update_theme_options_field' ); ?>
 			<p class="submit">
-				<input class="button-primary" type="submit" name="update_theme_options" value="<?php _e('Update settings', 'redirection'); ?>" /></p>
+				<input class="button-primary" type="submit" name="update_theme_options"
+				       value="<?php _e( 'Update settings', 'redirection' ); ?>"/></p>
 		</form>
 	</div>
 	<?php
--- index.php
+++ index.php
@@ -8,10 +8,16 @@
  */
 
 $redirect = get_option( 'redirection_url' );
+
+// Exec redirection only on production, otherwise only display a user message
+if ( defined( 'WP_ENV' ) && ! in_array( constant( 'WP_ENV' ), array( 'production', 'PROD' ) ) ) {
+	wp_die( sprintf( 'This theme make a 301 redirection on production to this URL: %s', $redirect ) );
+}
+
 if ( ! empty( $redirect ) ) {
-	/** Hack : striclty redirect to wanted url */
+	/** Hack : strictly redirect to wanted url */
 	wp_redirect( $redirect, 301, 'bea-redirection' );
 	exit;
-} 
+}
 
-wp_die( 'Configure theme redirection on BO please' );
+wp_die( sprintf( 'Please configure theme redirection on <a href="%s">back-end settings page</a>', admin_url( 'themes.php?page=redirection-theme-settings' ) ) );
